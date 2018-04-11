47514c07e888409013c3732f74884b6bacc3371c
JustinRudat <justin.rudat@etu.upmc.fr>
2018-04-12 01:21:19 +0200

    adding EditMap Implem

--- src/dungeonMaster/components/EditMapImplem.java
+++ src/dungeonMaster/components/EditMapImplem.java
@@ -4,43 +4,7 @@ import dungeonMaster.services.Cell;
 import dungeonMaster.services.EditMapService;
 import dungeonMaster.services.MapService;
 
-public class EditMapImplem implements EditMapService{
-
-	@Override
-	public int getHeight() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getWidth() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public Cell cellNature(int i, int j) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void init(int largeur, int hauteur) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public MapService openDoor(int x, int y) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public MapService closeDoor(int x, int y) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+public class EditMapImplem extends MapImplem implements EditMapService{
 
 	@Override
 	public boolean isReachable(int x1, int y1, int x2, int y2) {
@@ -59,5 +23,7 @@ public class EditMapImplem implements EditMapService{
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	
 
 }

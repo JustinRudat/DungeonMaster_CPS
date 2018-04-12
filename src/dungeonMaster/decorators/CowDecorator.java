5001eb3a55d7bec4e62049c2487f2f2307856aaf
JustinRudat <justin.rudat@etu.upmc.fr>
2018-04-12 22:16:36 +0200

    whoops forgot the cow

--- src/dungeonMaster/decorators/CowDecorator.java
+++ src/dungeonMaster/decorators/CowDecorator.java
@@ -1,7 +1,16 @@
 package dungeonMaster.decorators;
 
 import dungeonMaster.services.CowService;
+import dungeonMaster.services.Dir;
+import dungeonMaster.services.EnvironmentService;
+import dungeonMaster.services.PlayerService;
 
-public interface CowDecorator extends CowService {
+public class CowDecorator extends EntityDecorator implements CowService {
+	
+	public  CowDecorator(CowService delegate) {
+		super(delegate);
+	}
+	
+	
 
 }

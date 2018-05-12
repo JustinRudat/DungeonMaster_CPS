4492ecfc357f74856514d83f448686077924a9e8
JustinRudat <justin.rudat@etu.upmc.fr>
2018-05-12 17:29:59 +0200

    deletion of objectservice now lootservice

--- src/dungeonMaster/services/ObjetService.java
+++
@@ -1,9 +0,0 @@
-package dungeonMaster.services;
-
-public interface ObjetService {
-	public String getName();
-	public int getBonus();
-	public int getCol();
-	public int getRow();
-	public boolean init(int col, int row, int bonus, String name);
-}
